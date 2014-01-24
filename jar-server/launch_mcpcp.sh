5faf14fccd0d227a573f32f8ba1721638d05ade8
Damien Duboeuf <d.duboeuf@glowbl.com>
2014-01-24 17:59:25 +0100

    update script launch

--- jar-server/launch_mcpcp.sh
+++ jar-server/launch_mcpcp.sh
@@ -1,10 +1,22 @@
 #!/bin/sh
 
-cd ..
-./recompile.sh
-./reobfuscate_srg.sh
+cd $1
 
-cd reobf/minecraft/
-zip LTB.jar *
+if [ "$2" = "" ]; then
+	pwd
+	
+	cd ..
+	./recompile.sh
+	./reobfuscate_srg.sh
+	pwd
 
-cd ../../jar-server
+	cd reobf/minecraft/
+	zip LTB.jar *
+	mv LTB.jar ../../jar-server/mods/LTB.jar
+	pwd
+
+	cd ../../jar-server
+	pwd
+fi;
+java -jar mcpc-plus-1.6.4-R2.1-forge965-B224.jar
+
